0d4de73c1bfcbaa9efcf0dc4552d594a7e67f2b1
thuyvtk <vokimthuy211@gmail.com>
2020-07-29 11:07:42 +0700

    [ThuyVTK] Map dynamic data, update UI search bar (todo: clearble value input)

--- src/components/core_layout/navigationBar.vue
+++ src/components/core_layout/navigationBar.vue
@@ -21,86 +21,117 @@
       </v-text-field>
     </v-overlay>
     <v-app-bar app color="#fff" light height="70" id="top-bar">
-      <router-link to="/">
-        <v-img
-          alt="Hostel Renting"
-          class="shrink mr-2"
-          contain
-          src="@/assets/logo1.png"
-          transition="scale-transition"
-          max-width="100"
-          max-height="80"
-        />
-      </router-link>
-      <gmap-autocomplete
-        placeholder="Tìm theo địa chỉ, địa điểm..."
-        class="searchBar"
-        @place_changed="setPlace"
-      ></gmap-autocomplete>
-      <v-btn depressed @click="searchByCoordinates" color="primary" height="40">
-        <v-icon>search</v-icon>
-      </v-btn>
-      <v-btn
-        color="primary"
-        height="48"
-        depressed
-        icon
-        fab
-        class="ma-2 hidden-md-and-up"
-        @click="overlay.show = true"
-      >
-        <v-icon light>mdi-magnify</v-icon>
-      </v-btn>
+      <v-row height="70 m-0" class="d-flex">
+        <v-col class="col-7">
+          <v-row>
+            <v-col class="col-2">
+              <router-link to="/">
+                <v-img
+                  alt="Hostel Renting"
+                  class="shrink mr-2"
+                  contain
+                  src="@/assets/logo1.png"
+                  transition="scale-transition"
+                  max-width="100"
+                  max-height="80"
+                />
+              </router-link>
+            </v-col>
+            <!-- </v-col>
+            <v-col class="col-5">-->
+            <v-col class="col-10 pl-10">
+              <v-row class="px-0 d-flex align-center">
+                <v-col class="col-9 pl-5 searchBar d-flex align-center">
+                  <gmap-autocomplete
+                    placeholder="Tìm theo địa chỉ, địa điểm..."
+                    class="col-11 gmap-input"
+                    clearable
+                    clear-icon="cancel"
+                    @place_changed="setPlace"
+                    :value="address"
+                  ></gmap-autocomplete>
+                  <v-btn icon>
+                    <v-icon>clear</v-icon>
+                  </v-btn>
+                </v-col>
+                <v-col class="col-3 px-0">
+                  <v-btn
+                    class="rounded-l-0"
+                    depressed
+                    @click="searchByCoordinates"
+                    color="primary"
+                    height="40"
+                  >
+                    <v-icon>search</v-icon>
+                  </v-btn>
+                  <v-btn
+                    color="primary"
+                    height="48"
+                    depressed
+                    icon
+                    fab
+                    class="ma-2 hidden-md-and-up"
+                    @click="overlay.show = true"
+                  >
+                    <v-icon light>mdi-magnify</v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-col>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        color="#2C92D5"
-        light
-        depressed
-        outlined
-        rounded
-        class="ma-1 hidden-xs-only font-weight-regular"
-      >
-        <v-icon left>fas fa-paper-plane</v-icon>Đăng ký tìm phòng
-      </v-btn>
-
-      <v-btn color="#6C98C6" to="/cart" depressed dark class="ma-1 hidden-xs-only">Lịch hẹn của bạn</v-btn>
-      <v-menu>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon large class="ma-1" v-bind="attrs" v-on="on">
-            <v-avatar size="32px" item>
-              <v-img src="@/assets/suzy-avatar.jpg" alt="My Suzy"></v-img>
-            </v-avatar>
+        <v-col class="col-5 ml-auto d-flex justify-end items-center align-center pr-5">
+          <v-btn
+            color="#2C92D5"
+            light
+            depressed
+            outlined
+            rounded
+            class="mr-5 hidden-xs-only font-weight-regular"
+          >
+            <v-icon left>fas fa-paper-plane</v-icon>Đăng ký tìm phòng
           </v-btn>
-        </template>
-        <v-list>
-          <v-list-item to="/cart">
-            <v-list-item-icon>
-              <v-icon>far fa-user-circle</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Your profile</v-list-item-title>
-          </v-list-item>
-          <v-list-item to="/qr">
-            <v-list-item-icon>
-              <v-icon>fas fa-qrcode</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>QR Scanner</v-list-item-title>
-          </v-list-item>
-          <v-list-item to="#" class="hidden-sm-and-up">
-            <v-list-item-icon>
-              <v-icon>fas fa-paper-plane</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Room alert</v-list-item-title>
-          </v-list-item>
-          <v-list-item to="#" class="hidden-sm-and-up">
-            <v-list-item-icon>
-              <v-icon>far fa-bookmark</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Your Booking</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+
+          <!-- eslint-disable -->
+          <v-btn color="#6C98C6" to="/cart" depressed dark class="hidden-xs-only">Lịch hẹn của bạn</v-btn>
+          <v-menu>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon large class="ma-1 ml-5" v-bind="attrs" v-on="on">
+                <v-avatar size="32px" item>
+                  <v-img src="@/assets/suzy-avatar.jpg" alt="My Suzy"></v-img>
+                </v-avatar>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item to="/cart">
+                <v-list-item-icon>
+                  <v-icon>far fa-user-circle</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Your profile</v-list-item-title>
+              </v-list-item>
+              <v-list-item to="/qr">
+                <v-list-item-icon>
+                  <v-icon>fas fa-qrcode</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>QR Scanner</v-list-item-title>
+              </v-list-item>
+              <v-list-item to="#" class="hidden-sm-and-up">
+                <v-list-item-icon>
+                  <v-icon>fas fa-paper-plane</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Room alert</v-list-item-title>
+              </v-list-item>
+              <v-list-item to="#" class="hidden-sm-and-up">
+                <v-list-item-icon>
+                  <v-icon>far fa-bookmark</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Your Booking</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-col>
+      </v-row>
     </v-app-bar>
   </div>
 </template>
@@ -118,7 +149,10 @@
 .searchBar {
   height: 40px;
   border: 1px solid #e4e7ea;
-  width: 300px;
+  width: 100%;
+}
+.gmap-input:focus {
+  outline: none;
 }
 </style>
 <script>
@@ -136,13 +170,11 @@ export default {
   }),
   methods: {
     setPlace(place) {
+      this.isActive = !this.isActive;
       this.currentPlace = place;
     },
     searchByCoordinates() {
       if (this.currentPlace) {
-        alert(
-          `lat: ${this.currentPlace.geometry.location.lat()} ,lng: ${this.currentPlace.geometry.location.lng()}`,
-        );
         const coordinates = {
           lat: this.currentPlace.geometry.location.lat(),
           lng: this.currentPlace.geometry.location.lng(),
--- src/components/home/Article.vue
+++ src/components/home/Article.vue
@@ -1,25 +1,27 @@
 <template>
   <v-card class="mx-auto" :width="responsive.article.width">
     <v-row class="mr-0 ml-0">
-      <v-col cols="12" class="d-flex flex-xl-row flex-lg-row flex-md-column flex-sm-column
-      flex-column item-classic item-classic-horizontal">
+      <!-- eslint-disable -->
+      <v-col
+        cols="12"
+        class="d-flex flex-xl-row flex-lg-row flex-md-column flex-sm-column flex-column item-classic item-classic-horizontal"
+      >
+        <!-- eslint-enable -->
         <v-col cols="12" class="item-classic-inner d-flex justify-start">
           <v-col cols="4" class="item-classic-left left">
             <div class="item-classic-media">
-            <v-img
-              _:max-height="responsive.image.height"
-              _:max-width="responsive.image.width"
-              :src="'https://tiendoduan.org/wp-content/uploads/2020/04/can-ho-mini-25m2.jpg'"
-            >
-              <div class="top">
-                Top
-                <v-icon right color="white">{{numberIcons[index-1]}}</v-icon>
+              <v-img
+                :src="'https://tiendoduan.org/wp-content/uploads/2020/04/can-ho-mini-25m2.jpg'"
+              >
+                <div class="top">
+                  Top
+                  <v-icon right color="white">{{numberIcons[index-1]}}</v-icon>
+                </div>
+              </v-img>
+              <div class="item-classic-price">
+                <span>{{type.price}} {{type.priceUnit}}</span>
               </div>
-            </v-img>
-            <div class="item-classic-price">
-              <span>{{type.price}} {{type.priceUnit}}</span>
             </div>
-          </div>
           </v-col>
           <v-col cols="8" class="right d-flex align-center justify-center">
             <v-col cols="11">
@@ -71,6 +73,8 @@
                   max-height="20"
                 />
                 <span class="item-text">3k/kwh</span>
+                <!-- eslint-disable -->
+                <!-- <span class="item-text">{{services.find(s=> s.name === "internet").price}}</span> -->
                 <div class="ml-auto d-flex">
                   <v-img
                     class="shrink mr-4 ml-auto"
@@ -80,6 +84,8 @@
                     max-height="25"
                   />
                   <span class="item-text">50k/người</span>
+                  <!-- eslint-disable -->
+                  <!-- <span class="item-text">{{services.find(s=> s.name === "nước").price}}</span> -->
                 </div>
                 <div class="ml-auto d-flex">
                   <v-img
@@ -89,7 +95,7 @@
                     max-width="25"
                     max-height="25"
                   />
-                  <span class="item-text">50k/phòng</span>
+                  <span class="item-text">{{services.find(s=> s.name === "internet").price}}</span>
                 </div>
               </div>
               <div class="mt-3 d-flex align-center">
@@ -102,127 +108,21 @@
                 </div>
               </div>
               <v-card-actions class="pl-0 mt-3">
-                <v-btn color="#484848" class="btnDetail pl-3 pr-3 ml-auto"
+                <v-btn
+                  color="#484848"
+                  class="btnDetail pl-3 pr-3 ml-auto"
                   :to="'/detail/' + type.typeId"
-                >
-                  Chi tiết
-                </v-btn>
+                >Chi tiết</v-btn>
               </v-card-actions>
             </v-col>
           </v-col>
         </v-col>
       </v-col>
     </v-row>
-    <!-- <div class="d-flex flex-xl-row flex-lg-row flex-md-column flex-sm-column flex-column
-    item-classic item-classic-horizontal">
-      <div class="item-classic-inner d-flex"> -->
-        <!-- <div class="item-classic-left">
-          <div class="item-classic-media">
-            <v-img
-              :max-height="responsive.image.height"
-              :max-width="responsive.image.width"
-              :src="'https://tiendoduan.org/wp-content/uploads/2020/04/can-ho-mini-25m2.jpg'"
-            >
-              <div class="top">
-                Top
-                <v-icon right color="white">{{numberIcons[index-1]}}</v-icon>
-              </div>
-            </v-img>
-            <div class="item-classic-price">
-              <span>1.500.000 đ</span>
-            </div>
-          </div>
-        </div> -->
-        <!-- <v-row class="d-flex">
-          <v-col cols="10" class="red">
-            <div class="item-classic-right pl-5">
-              <div class="type-name">
-                <p class="font-weight-bold mb-0">Ký túc xá đại học quốc gia</p>
-              </div>
-          </div>
-          </v-col>
-        </v-row>
-      </div> -->
-      <!-- <div class="right d-flex flex-column">
-        <div class="d-flex justify-space-center align-center type-name flex-xl-row">
-          <v-card-title>{{group.groupName}} - {{type.typeName}}</v-card-title>
-          <v-btn outlined :to="'/detail/' + type.typeId">Chi tiết</v-btn>
-        </div>
-        <v-card-subtitle class="amber--text font-weight-medium"> -->
-          <!-- <v-icon>mdi-currency-usd-circle-outline</v-icon>{{type.price}} {{type.priceUnit}} -->
-        <!-- </v-card-subtitle>
-        <div class="ml-4" style="border: 1px solid #6C98C6; width: 200px"></div>
-        <div class="d-flex align-center justify-space-between">
-          <span class="mx-2 mt-4">
-            <p>
-              <v-icon class="mx-2" x-small>fas fa-map-marker-alt</v-icon>{{group.street}}
-            </p>
-          </span>
-        </div>
-        <div class="d-flex align-center justify-space-between flex-wrap">
-          <span class="mx-2">
-            <p class="success--text">
-              <v-icon class="mx-2" x-small>fas fa-store</v-icon>Chợ Hoàng Hoa Thám
-            </p>
-          </span>
-          <span class="mx-2">
-            <p>
-              <v-icon class="mx-2" x-small>fas fa-ruler</v-icon>0.2 Km
-            </p>
-          </span>
-        </div>
-        <div class="d-flex align-center justify-space-between">
-          <span class="mx-2">
-            <v-icon class="mx-2" x-small>fas fa-ruler-combined</v-icon>20m2
-          </span>
-          <span class="mx-2">
-            <v-icon class="mx-2" x-small>fas fa-users</v-icon>
-            {{type.capacity}} người
-          </span>
-          <span class="mx-2">
-            <v-icon class="mx-2" x-small>mdi-toilet</v-icon>Dùng riêng
-          </span>
-        </div>
-        <div class="d-flex align-center justify-space-between mt-4">
-          <span class="mx-2">
-            <v-icon class="mx-2" x-small>fas fa-bolt</v-icon>3.5k/Kwh
-          </span>
-          <span class="mx-2">
-            <v-icon class="mx-2" x-small>fas fa-tint</v-icon>50k/người
-          </span>
-          <span class="mx-2">
-            <v-icon class="mx-2" x-small>mdi-wifi</v-icon>Miễn phí
-          </span>
-        </div>
-        </div>
-        </div>
-        <div class="d-flex align-center justify-space-between mt-2 ml-2">
-          <v-chip-group class center-active light show-arrows mandatory>
-            <v-chip outlined>
-              <v-icon class="mr-2" x-small>mdi-weather-windy</v-icon>May Lanh
-            </v-chip>
-            <v-chip outlined>
-              <v-icon class="mr-2" x-small>mdi-fridge</v-icon>Tu Lanh
-            </v-chip>
-            <v-chip outlined>
-              <v-icon class="mr-2" x-small>fas fa-motorcycle</v-icon>Cho dau xe
-            </v-chip>
-            <v-chip outlined>
-              <v-icon class="mr-2" x-small>mdi-washing-machine</v-icon>May giat
-            </v-chip>
-          </v-chip-group>
-        </div>
-      </div> -->
-    <div style="background-color: #f3f4f9">
+    <!-- <div style="background-color: #f3f4f9">
       <div class="ml-2" style="background-color: #e7e9f0;">
         <div class="d-flex align-center justify-space-between ml-2 pt-2 pb-2">
-          <v-chip-group
-            class=""
-            center-active
-            light
-            show-arrows
-            mandatory
-          >
+          <v-chip-group class center-active light show-arrows mandatory>
             <div class="d-flex align-center pl-7 pr-7">
               <v-img
                 class="shrink mr-2"
@@ -253,87 +153,84 @@
               />
               <span class="equipment-name ml-2">Chỗ đậu xe</span>
             </div>
-            <!-- </v-chip> -->
-            <!-- <v-chip>
-              <v-icon class="mr-2" x-small>mdi-fridge</v-icon>Tu Lanh
-            </v-chip>
-            <v-chip>
-              <v-icon class="mr-2" x-small>fas fa-motorcycle</v-icon>Cho dau xe
-            </v-chip>
-            <v-chip>
-              <v-icon class="mr-2" x-small>mdi-washing-machine</v-icon>May giat
-            </v-chip> -->
           </v-chip-group>
         </div>
       </div>
-    </div>
+    </div>-->
   </v-card>
 </template>
 <style scoped>
-  .item-classic {
-    position: relative;
-    overflow: hidden !important;
-    z-index: 1;
-    letter-spacing: 0;
-    transition: .15s ease-out all;
-    backface-visibility: hidden;
-    min-height: 250px;
-  }
-  .v-card:hover {
-    box-shadow: 0 0 29px 0 rgba(0, 0, 0, 0.16) !important;
-  }
-  .item-classic-horizontal {
-    padding: 0;
-    box-shadow: none;
-    overflow: visible !important;
-    text-align: left;
-  }
-  .item-classic-horizontal .item-classic-inner {
-    position: relative;
-  }
-  .item-classic-left {
-    display: flex;
-    /* width: 100%; */
-    flex-shrink: 0;
-    /* max-width: 265px; */
-    background-color: #f3f4f9;
-    padding: 0;
-  }
-  .v-responsive__sizer {
-    padding-bottom: 100% !important;
-  }
-  .item-classic-price {
-    left: 0 !important;
-    top: 28px;
-    bottom: auto !important;
-    z-index: 100;
-    position: absolute;
-    padding: 2px 21px 2px 11px;
-    font-family: "Poppins", sans-serif;
-    color: #ffffff;
-    background-color: #2170B9;
-  }
-  .item-classic-price::before {
-    content: '';
-    position: absolute;
-    top: 100%;
-    left: 0;
-    width: 0;
-    height: 0;
-    border-style: solid;
-    border-width: 0 8px 8px 0;
-    border-color: transparent #367dc3 transparent transparent;
+.item-classic {
+  position: relative;
+  overflow: hidden !important;
+  z-index: 1;
+  letter-spacing: 0;
+  transition: 0.15s ease-out all;
+  backface-visibility: hidden;
+  min-height: 250px;
 }
-  .item-classic-media {
-    position: relative;
-    padding-left: 7px;
-    overflow: hidden;
-  }
-  .v-sheet.v-card:not(.v-sheet--outlined) {
-    box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.14),
-    0px 0px 0px 0px rgba(0, 0, 0, 0.14),
-    0px 0px 0px 0px rgba(0, 0, 0, 0.12);
+.v-card:hover {
+  box-shadow: 0 0 29px 0 rgba(0, 0, 0, 0.16) !important;
 }
+.item-classic-horizontal {
+  padding: 0;
+  box-shadow: none;
+  overflow: visible !important;
+  text-align: left;
+}
+.item-classic-horizontal .item-classic-inner {
+  position: relative;
+}
+.item-classic-left {
+  display: flex;
+  /* width: 100%; */
+  flex-shrink: 0;
+  /* max-width: 265px; */
+  background-color: #f3f4f9;
+  padding: 0;
+}
+.v-responsive__sizer {
+  padding-bottom: '100%' !important;
+}
+.item-classic-price {
+  left: 0 !important;
+  top: 28px;
+  bottom: auto !important;
+  z-index: 100;
+  position: absolute;
+  padding: 2px 21px 2px 11px;
+  font-family: 'Poppins', sans-serif;
+  color: #ffffff;
+  background-color: #2170b9;
+}
+.item-classic-price::before {
+  content: '';
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 0 8px 8px 0;
+  border-color: transparent #367dc3 transparent transparent;
+}
+.item-classic-media {
+  position: relative;
+  padding-left: 7px;
+  overflow: hidden;
+}
+.v-sheet.v-card:not(.v-sheet--outlined) {
+  box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.14),
+    0px 0px 0px 0px rgba(0, 0, 0, 0.14), 0px 0px 0px 0px rgba(0, 0, 0, 0.12);
+}
+.v-img {
+  background-size: contain, cover !important;
+}
+
+.v-responsive__content {
+  padding-bottom: 100% !important;
+}
+
 .item-equipment {
   background-color: #e7e9f0;
 }
@@ -345,7 +242,7 @@
   width: auto;
   max-width: 80%;
   padding: 8px 16px;
-  background:  rgba(32, 112, 185, 0.66);
+  background: rgba(32, 112, 185, 0.66);
   color: #fff;
   text-align: center;
   line-height: 22px;
@@ -385,7 +282,7 @@
   font-family: 'Nunito';
 }
 .v-btn:hover {
-  background-color: #6C98C6 !important;
+  background-color: #6c98c6 !important;
 }
 </style>
 <script>
@@ -417,6 +314,12 @@ export default {
   computed: {
     isLoadingProvinces() {
       return this.$store.state.renter.common.provinces.isLoading;
+    },
+    services() {
+      return this.type.services.map((s) => ({
+        name: s.serviceName,
+        price: `${s.servicePrice} ${s.priceUnit}/${s.userUnit}`,
+      }));
     },
     res() {
       return this.$vuetify.breakpoint.name;
