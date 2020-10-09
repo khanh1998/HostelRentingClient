4a92e724a06b99452475e6986ec3286143f1ff99
Bui Quoc Khanh <quockhanh.abc@gmail.com>
2020-10-09 09:41:35 +0700

    modify place picker

--- src/components/vendor/hostel_management/PlacePicker.vue
+++ src/components/vendor/hostel_management/PlacePicker.vue
@@ -3,7 +3,8 @@
     <div>
       <h2>Chọn vị trí trên bản đồ</h2>
       <label>
-        <gmap-autocomplete @place_changed="setPlace"> </gmap-autocomplete>
+        <gmap-autocomplete @place_changed="setPlace" :options="autocompleteOptions">
+        </gmap-autocomplete>
         <button @click="addMarker"><v-icon>push_pin</v-icon></button>
       </label>
       <br />
@@ -15,6 +16,7 @@
         @click="center = marker.position"
         :clickable="true"
         :draggable="true"
+        :title="currentAddressString"
       ></gmap-marker>
     </gmap-map>
   </div>
@@ -29,7 +31,10 @@ export default {
       center: { lat: 10.8230989, lng: 106.6296638 },
       marker: { position: { lat: 10.8230989, lng: 106.6296638 } },
       place: { position: { lat: 10.8230989, lng: 106.6296638 } },
-      currentPlace: null,
+      addressString: 'Thành phố Hồ Chí Minh',
+      autocompleteOptions: {
+        components: 'country:vi',
+      },
     };
   },
 
@@ -40,28 +45,32 @@ export default {
   methods: {
     // receives a place object via the autocomplete component
     setPlace(place) {
-      this.currentPlace = place;
+      this.place = place;
+      this.addMarker();
+      this.addressString = `${this.place.formatted_address}`;
     },
     addMarker() {
-      if (this.currentPlace) {
-        const marker = {
-          lat: this.currentPlace.geometry.location.lat(),
-          lng: this.currentPlace.geometry.location.lng(),
-        };
-        this.marker = { position: marker };
-        this.place = this.currentPlace;
-        this.center = marker;
-        this.currentPlace = null;
-      }
+      const marker = {
+        lat: this.place.geometry.location.lat(),
+        lng: this.place.geometry.location.lng(),
+      };
+      this.marker = { position: marker };
+      this.center = marker;
     },
     geolocate() {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.center = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-      });
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          console.log(position);
+          const coord = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          this.center = coord;
+          this.place = { position: coord };
+        });
+      }
     },
   },
+  computed: {},
 };
 </script>
