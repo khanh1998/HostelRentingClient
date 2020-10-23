40bd41bca52efe5cdca070e146b978401da1cc45
Bui Quoc Khanh <quockhanh.abc@gmail.com>
2020-10-23 16:48:40 +0700

    user profile, temporary commit

--- src/views/renter/User.vue
+++ src/views/renter/User.vue
@@ -24,43 +24,68 @@
           <v-avatar size="128">
             <v-img :src="user.data.avatar" />
           </v-avatar>
-          <v-btn color="primary" @click="prepareToEditProfile">
+          <v-btn color="primary" v-if="!editProfile" @click="prepareToEditProfile">
             <v-icon>edit</v-icon> Sửa hồ sơ
+          </v-btn>
+          <v-btn color="primary" v-if="editProfile" @click="editProfile = false">
+            <v-icon>cancel</v-icon> Hủy
           </v-btn>
         </v-col>
         <v-col cols="3">
-          <p v-if="!editProfile">{{ user.data.username }}</p>
-          <p v-if="!editProfile">{{ user.data.role.roleName }}</p>
-          <v-text-field v-if="editProfile" v-model="newUser.username"></v-text-field>
-          <p v-if="!editProfile">{{ user.data.yearOfBirth }}</p>
+          <p v-if="!editProfile">{{ user.data.role.roleName }}: {{ user.data.username }}</p>
+          <v-text-field v-if="editProfile" label="Tên" v-model="newUser.username"></v-text-field>
+          <p v-if="!editProfile">Năm sinh: {{ user.data.yearOfBirth }}</p>
           <v-text-field
             v-if="editProfile"
             type="number"
+            label="Năm sinh"
             v-model="newUser.yearOfBirth"
           ></v-text-field>
-          <p v-if="!editProfile">{{ user.data.phone }}</p>
-          <p v-if="!editProfile">{{ user.data.email }}</p>
-          <v-text-field v-if="editProfile" type="email" v-model="newUser.email"></v-text-field>
+          <p v-if="!editProfile">Số điện thoại: {{ user.data.phone }}</p>
+          <p v-if="!editProfile">Email: {{ user.data.email }}</p>
+          <v-text-field v-if="editProfile" label="Email" v-model="newUser.email"></v-text-field>
         </v-col>
       </v-row>
       <v-row>
         <!-- CMND -->
         <v-col>
-          <p v-if="!editProfile">{{ user.data.idNum }}</p>
-          <v-text-field v-if="editProfile" type="number" v-model="newUser.idNum"></v-text-field>
-          <p v-if="!editProfile">{{ user.data.idIssuedLocation }}</p>
-          <v-text-field v-if="editProfile" v-model="newUser.idIssuedLocation"></v-text-field>
-          <p v-if="!editProfile">{{ user.data.idIssuedDate }}</p>
-          <v-text-field v-if="editProfile" v-model="newUser.idIssuedDate"></v-text-field>
-          <p v-if="!editProfile">{{ user.data.householdAddress }}</p>
-          <v-text-field v-if="editProfile" v-model="newUser.householdAddress"></v-text-field>
-          <p v-if="!editProfile">{{ user.data.currentAddress }}</p>
-          <v-text-field v-if="editProfile" v-model="newUser.currentAddress"></v-text-field>
+          <p>Chứng minh nhân dân</p>
+          <p v-if="!editProfile">Số chứng minh nhân dân: {{ user.data.idNum }}</p>
+          <v-text-field
+            v-if="editProfile"
+            label="Số chứng minh nhân dân"
+            type="number"
+            v-model="newUser.idNum"
+          ></v-text-field>
+          <p v-if="!editProfile">Nơi cấp: {{ user.data.idIssuedLocation }}</p>
+          <v-text-field
+            v-if="editProfile"
+            label="Nơi cấp"
+            v-model="newUser.idIssuedLocation"
+          ></v-text-field>
+          <p v-if="!editProfile">Thời gian cấp:{{ user.data.idIssuedDate }}</p>
+          <v-text-field
+            v-if="editProfile"
+            label="Thời gian cấp"
+            v-model="newUser.idIssuedDate"
+          ></v-text-field>
+          <p v-if="!editProfile">Địa chỉ thường trú: {{ user.data.householdAddress }}</p>
+          <v-text-field
+            v-if="editProfile"
+            label="Địa chỉ thường trú"
+            v-model="newUser.householdAddress"
+          ></v-text-field>
+          <p v-if="!editProfile">Địa chỉ hiện tại: {{ user.data.currentAddress }}</p>
+          <v-text-field
+            v-if="editProfile"
+            label="Địa chỉ hiện tại"
+            v-model="newUser.currentAddress"
+          ></v-text-field>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
-          <v-btn v-if="updatable" @click="updateUser" color="primary">
+          <v-btn v-if="updatable && editProfile" @click="updateUserInfo" color="primary">
             <v-icon>update</v-icon> Cập nhật ngay
           </v-btn>
         </v-col>
@@ -83,7 +108,7 @@ export default {
     prepareToEditProfile() {
       this.editProfile = true;
     },
-    updateUser() {
+    updateUserInfo() {
       this.updateUser(this.newUser).then(() => {
         if (this.user.error) {
           this.showSnackBar(this.user.error.message, { color: 'red' });
@@ -113,7 +138,7 @@ export default {
   },
   created() {
     this.getUser().then(() => {
-      this.newUser = this.user.data;
+      this.newUser = { ...this.user.data };
     });
   },
 };
